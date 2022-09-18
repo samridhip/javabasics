db4753bf9e8690ac9434f78b71f199d7b84c2ee6
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-18 13:08:49 +0530

    added first name and last name

---
+++ src/name.java
@@ -0,0 +1,8 @@
+public class name {
+    public static void main(String[]args){
+        String firstname = "samridhi ";
+        String lastname = "pandey";
+        String fullname = firstname+lastname;
+        System.out.print(fullname);
+    }
+}
