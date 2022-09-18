c1494dc4380491627bdfac1885755a54c5214c9d
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-18 13:14:42 +0530

    product of decimal

---
+++ src/product.java
@@ -0,0 +1,10 @@
+public class product {
+    public static void main(String[] args) {
+        float num1 = 5.5f;
+        float num2 = 6.6f;
+        float product = num1*num2;
+        System.out.println(product);
+
+
+    }
+}
