b0368c0c7fad9d1050da78c427e03eebbb911e80
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-18 23:37:55 +0530

    Simple Interest

---
+++ src/SimpleInterest.java
@@ -0,0 +1,9 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        int principle = 300000;
+        int rate = 5;
+        int time = 3;
+        int si = principle*rate*time/100;
+        System.out.print(si);
+    }
+}
