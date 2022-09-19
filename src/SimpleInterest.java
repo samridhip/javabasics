798d047484310ed12e61df41addf72c69dbd967a
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-19 22:31:06 +0530

    name

--- src/SimpleInterest.java
+++ src/SimpleInterest.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 public class SimpleInterest {
     public static void main(String[] args) {
-        int principle = 300000;
-        int rate = 5;
-        int time = 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Principle:");
+        int principle = sc.nextInt() ;
+        System.out.println("Enter Rate:");
+        int rate = sc.nextInt();
+        System.out.println("Enter Time:");
+        int time = sc.nextInt();
+        System.out.println("Simple Interest = ");
         int si = principle*rate*time/100;
         System.out.print(si);
     }
