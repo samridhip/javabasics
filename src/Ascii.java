22bccd75d5248308b672973747a05efa49d8791b
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-21 00:40:54 +0530

    ascii value of characters

---
+++ src/Ascii.java
@@ -0,0 +1,8 @@
+public class Ascii {
+    public static void main(String[]args){
+        char val = 'a'+1;
+        int val2 = 'a'+1;
+        System.out.println(val);
+        System.out.println(val2);
+    }
+}
