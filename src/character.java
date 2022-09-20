a9061f7b59b3aab3d47f9731214e2e019f7325d6
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-21 00:24:56 +0530

    input of character

---
+++ src/character.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class character {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("enter a chatacter");
+        char a = input.next().charAt(0);
+        System.out.println(a);
+
+
+
+    }
+
+
+}
