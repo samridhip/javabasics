140702bab7ccdc3224350345f56e8b68c9d4ca15
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-21 14:06:03 +0530

    short conversation

---
+++ src/Practice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("what is your name" );
+
+    }
+}
---
+++ src/sss.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sss {
+    public static void main(String[] args) {
+        System.out.println("where are you?");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println("why?");
+        String b = sc.next();
+        System.out.println("ok");
+
+
+    }
+}
