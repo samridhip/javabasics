efabb13d6e53c5da4847976aa196043baa654ab5
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-21 18:51:15 +0530

    practice question 1

---
+++ src/question1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class question1 {
+    public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);
+        System.out.println("Enter first number" );
+        int a = ss.nextInt();
+        System.out.println("Enter second number");
+        int b = ss.nextInt();
+        System.out.println(a+b);
+        System.out.println(a*b);
+
+    }
+}
