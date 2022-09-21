52394702422b8a45d670a5b6106b9cd7db31579a
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-21 18:30:31 +0530

    ordering in cafe

---
+++ src/questions.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class questions {
+    public static void main(String[] args) {
+        System.out.println("can i take your order?");
+        Scanner ss = new Scanner(System.in);
+        String a = ss.next();
+        System.out.println("anything else ma'am");
+        String b = ss.next();
+        System.out.println("It will take time");
+        String c = ss.next();
+        System.out.println("thanks for visiting");
+
+
+
+
+    }
+}
