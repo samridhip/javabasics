69e0989876a3800dc0db438c89697ec7a4d41f4c
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-21 22:37:29 +0530

    operation

---
+++ src/unary.java
@@ -0,0 +1,13 @@
+public class unary {
+    public static void main(String[] args) {
+        int a = 100;
+         int b = a++;
+         int c = a+1;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(a);
+        System.out.println((++a) + a--);
+
+    }
+
+}
