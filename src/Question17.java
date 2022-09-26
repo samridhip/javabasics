f27f5f2a8e8eb3bfe819766109eac6f728d438df
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-26 10:49:14 +0530

    for, while and do loop

---
+++ src/Question17.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Question17 {
+    public static void main(String[] args) {
+        Scanner ss = new Scanner(System.in);
+        System.out.println();
+        for(int i=0;i<5;i=i+1){
+                System.out.println(i);
+        }
+        System.out.println("Success");
+    }
+}
