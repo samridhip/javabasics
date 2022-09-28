6bbb75ef2c1efae08845e860af6b20f7bd64e214
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-28 22:16:43 +0530

    array

---
+++ src/Question26.java
@@ -0,0 +1,7 @@
+public class Question26 {
+    public static void main(String[] args) {
+        String[] Fruits={"Apple","Banana","Kiwi","Mausambi","Mango",};
+        for(int i =0;i<Fruits.length;i++)
+        {System.out.println(Fruits[i]);
+    }
+}}
