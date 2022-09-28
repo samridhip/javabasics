461886a923744d9751eba2f0d3e56051ac91a5af
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-28 22:09:39 +0530

    array

---
+++ src/Question25.java
@@ -0,0 +1,7 @@
+public class Question25 {
+    public static void main(String[] args) {
+        int[] array= {3,4,5,7,88,90};
+        for(int i = 0;i<array.length;i++)
+        {System.out.println(array[i]);
+    }
+         }}
