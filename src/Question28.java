84853f1583d4bd2f8173dc6070a131a898fad54c
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-30 17:30:53 +0530

    array

---
+++ src/Question28.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Question28 {
+    public static void main(String[] args) {
+        int[] marks = new int[10];
+        marks[0]=12;
+        marks[1]=33;
+        marks[2]=44;
+        marks[3]=66;
+        marks[4]=86;
+        marks[5]=48;
+        marks[6]=77;
+        marks[7]=99;
+        marks[8]=49;
+        marks[9]=94;
+        System.out.println(marks[7]);
+
+
+
+
+
+
+    }}
