02961a221f292062e6e5002457b72b2e9194e637
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-30 23:12:36 +0530

    array in reverse order

---
+++ src/Arraypractice.java
@@ -0,0 +1,10 @@
+public class Arraypractice {
+    public static void main(String[] args) {
+        int[] marks = {55,89,90,60,75};
+        System.out.println("Printing using reverse method");
+        for(int i= marks.length-1;i>=0;i--){
+            System.out.println(marks[i]);
+        }
+
+    }
+}
