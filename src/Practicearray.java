f744f4f1c2c0a7ee94a2ad21223e43c6377cd6fa
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-09-30 19:59:19 +0530

    array

--- src/Practicearray.java
+++ src/Practicearray.java
@@ -2,5 +2,8 @@ public class Practicearray {
     public static void main(String[] args) {
         int[] marks = {44,85,49,80,38};
         System.out.println(marks.length);
+        for(int i = 0;i<marks.length;i++){
+            System.out.println(marks[i]);
+        }
     }
 }
