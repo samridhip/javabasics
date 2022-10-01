6148914f978f52576cf7ca7b7cf0b4e047e6582f
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-10-01 16:05:30 +0530

    array

--- src/Arraypractice.java
+++ src/Arraypractice.java
@@ -1,9 +1,8 @@
 public class Arraypractice {
     public static void main(String[] args) {
         int[] marks = {55,89,90,60,75};
-        System.out.println("Printing using reverse method");
-        for(int i= marks.length-1;i>=0;i--){
-            System.out.println(marks[i]);
+        for(int element:marks){
+            System.out.println(element);
         }
 
     }
---
+++ src/Multidimensionalarray.java
@@ -0,0 +1,17 @@
+public class Multidimensionalarray {
+    public static void main(String[] args) {
+        int[] marks = {45,60,70,60,79,90};
+        int[] grade = new int[6];
+        for(int i = 0;i<marks.length;i++){
+            grade[i] = marks[i];
+        }
+        
+        for(int i=0;i<grade.length;i++){
+            System.out.println(grade[i]);
+        }
+
+    }
+
+
+
+}
