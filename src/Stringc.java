0ff35f29e1e60fc00053b1113d1efe93277e0571
Samridhi Pandey <pandeysamridhi1@gmail.com>
2022-10-03 23:56:55 +0530

    array

---
+++ src/Stringc.java
@@ -0,0 +1,7 @@
+public class Stringc {
+    public static void main(String[] args) {
+        int b = 30;
+        String a = String.format("Age is %5d of vibhav " ,b);
+        System.out.println(a);
+    }
+}
